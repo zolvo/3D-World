5e0f6690facec202f3ee174cba7203cacc875bc1
zolvo <asicomindo@gmail.com>
2022-01-13 00:07:26 -0600

    update

---
+++ app.js
@@ -0,0 +1,36 @@
+function main() {
+  // scene camera renderer - 3 must!
+
+  const canvas = document.querySelector("#c");
+
+  const fov = 50;
+  // const aspect = 2;
+  const aspect = canvas.clientWidth / canvas.clientHeight;
+  const near = 0.1;
+  const far = 2000;
+
+  //   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  //   camera.position.z = 1;
+
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+
+  render.setSize(width, height);
+
+  const scene = new THREE.Scene();
+  const loader = new THREE.TextureLoader();
+  const texture = loader.load(
+    "https://threejs.org/manual/examples/resources/images/equirectangularmaps/tears_of_steel_bridge_2k.jpg",
+    () => {
+      const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
+      rt.fromEquirectangularTexture(rendere, texture);
+      scene.background = rt.texture;
+    }
+  );
+
+  function render() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  }
+}
